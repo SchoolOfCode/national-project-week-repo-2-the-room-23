b7b69972516120afcdc85803165b9fd0f427ba9b
Alex Platt <alja.platt@gmail.com>
2022-01-18 17:08:31 +0000

    added img folder and soc logo file

--- src/components/App/Index.js
+++ src/components/App/Index.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Header from "../Header";
 
-
 function App() {
-  return <div className="App">
-<Header></Header>
-  </div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
