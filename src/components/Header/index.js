d571968d8a3a04bd544018c3e6201d4d5357d353
elena souto <elenasouto@gmail.com>
2022-01-18 16:43:31 +0000

    index completed

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return ( 
+   <header>
+       <h1>SOC Quiz</h1>
+       <img src="" alt="image school of code" />
+   </header> 
+     );
+}
+ 
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
+import App from "./components/App/Index";
 
 ReactDOM.render(
   <React.StrictMode>
