021ddf33149daf5b9adfb0082c41a1d05c988153
Alex Platt <alja.platt@gmail.com>
2022-01-19 17:14:10 +0000

    merge

--- src/components/App/Index.js
+++ src/components/App/Index.js
@@ -1,17 +1,9 @@
 import { useEffect } from "react";
 import { API_URL } from "../../config/index.js";
 import "./App.css";
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import Header from "../Header";
 import Introduction from "../Introduction/index.js";
-=======
-
-import { Routes, Route } from "react-router-dom";
-import Header from "../Header";
-import Introduction from "../Introduction/index.js";
-
->>>>>>> f8caa707f6d56ccba437ed37776beedaaa0d6bff
 
 function App() {
   useEffect(() => {
@@ -25,11 +17,6 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
-=======
-
-     <Introduction/>
->>>>>>> f8caa707f6d56ccba437ed37776beedaaa0d6bff
       <Header />
       <Routes>
         <Route path="/" element={<Introduction />} />
