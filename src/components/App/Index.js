ac028f59ca6f65aed51bc29ccc9d951ad0b4faea
Ruben Gonzalez <rubengon.uk@gmail.com>
2022-01-19 10:20:19 +0000

    test 1

--- src/components/App/Index.js
+++ src/components/App/Index.js
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
-
+import { API_URL } from "../../config/index.js";
 import "./App.css";
 import Header from "../Header";
 
-const API_URL = process.env.REACT_APP_API_URL;
-
 function App() {
+
+  
   useEffect(() => {
     async function getData() {
-      const response = await fetch(`/questions`);
+      console.log(API_URL);
+      const response = await fetch(`${API_URL}/questions`,{
+    mode: 'no-cors' });
+      console.log(response);
       const data = await response.json();
       console.log(data);
     }
