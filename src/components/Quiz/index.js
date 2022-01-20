e5f8460d9bf951a9865630865e0476aaa207f758
Alex Platt <alja.platt@gmail.com>
2022-01-20 13:45:19 +0000

    merge to main - added question component, home link and add questions

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,15 +1,21 @@
 import QuestionText from "../QuestionText";
-import Answers from '../Answers/index';
-import SubmitButton from '../SubmitButton/index'
+import Answers from "../Answers/index";
+import SubmitButton from "../SubmitButton/index";
+import { Link } from "react-router-dom";
 
-function Quiz({actualQuestion, position}){
-    return ( 
-      <div>
-        <QuestionText  text={actualQuestion.question} id={actualQuestion.id} />
-        <Answers  answer={actualQuestion.answer} actA={actualQuestion.opta} actB={actualQuestion.optb} actC={actualQuestion.optc}  />
-        <SubmitButton/>
-      </div>
-    )
+function Quiz({ actualQuestion, position }) {
+  return (
+    <div>
+      <QuestionText text={actualQuestion.question} id={actualQuestion.id} />
+      <Answers
+        answer={actualQuestion.answer}
+        actA={actualQuestion.opta}
+        actB={actualQuestion.optb}
+        actC={actualQuestion.optc}
+      />
+      <SubmitButton />
+      <Link to="/">Home</Link>
+    </div>
+  );
 }
 export default Quiz;
- 
