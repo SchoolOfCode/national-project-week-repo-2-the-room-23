a51a473e5ffd171c841da89a5f4cf8a180ef7f40
Alex Platt <alja.platt@gmail.com>
2022-01-21 17:11:00 +0000

    small changes to comments, removed unnecessary css

--- src/components/Answers/index.js
+++ src/components/Answers/index.js
@@ -1,24 +1,23 @@
 import "./Answers.css";
-import Answer from "../Answer/index.js"
+import Answer from "../Answer/index.js";
 
-// Function for representing and answers list for every answer
-function Answers({handleAnswerValue, handleResult, disable, randomArray }){
-
-return(
-       <ul>
-          {randomArray.map( (element,index) => {
-             return( 
-               <Answer handleAnswerValue={handleAnswerValue}
-               className="white"
-               handleResult={handleResult}
-               text={element} 
-               disable={disable}
-               key={index}
-               />
-            )
-          })
-          }
-       </ul>
-    )
+// Function for representing an answers list for every answer
+function Answers({ handleAnswerValue, handleResult, disable, randomArray }) {
+  return (
+    <ul>
+      {randomArray.map((element, index) => {
+        return (
+          <Answer
+            handleAnswerValue={handleAnswerValue}
+            className="white"
+            handleResult={handleResult}
+            text={element}
+            disable={disable}
+            key={index}
+          />
+        );
+      })}
+    </ul>
+  );
 }
 export default Answers;
--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -7,27 +7,25 @@ import "./MainContent.css";
 
 // MainContent component which it's showing the questions and answers of aour game
 const MainContent = ({ topic, handleAddPoints, handleResetResult }) => {
-  // State for holding the questions data from the back-end claud database
+  // State for holding the questions data from the back-end cloud database
   const [questionsArray, setQuestionsArray] = useState([]);
-  
+
   // State for holding the actual question representing
   const [actualQuestion, setActualQuestion] = useState({});
-  
+
   // State for holding the position of the actual question in the questionsArray
   const [position, setPosition] = useState(1);
 
-  // State holding a boolean parameter for disable the click answers
-  const [disable, setDisable] = useState(false); 
+  // State holding a boolean parameter to disable click answers
+  const [disable, setDisable] = useState(false);
 
+  const [color, setColor] = useState("");
 
-  const [color , setColor] = useState("");
+  // State for holding the array of answers to show
+  const [answerArray, setAnswerArray] = useState([]);
 
-  // State for holding the array os answers to chow
-  const [answerArray , setAnswerArray] = useState([]);
-
-  // Function for getting the data from the back-end depends of the Topic end-point 
+  // Function for getting data from the back-end depends on the Topic end-point
   useEffect(() => {
-    
     async function getData(topic) {
       const response = await fetch(`${API_URL}/questions/${topic}`);
       const data = await response.json();
@@ -37,7 +35,6 @@ const MainContent = ({ topic, handleAddPoints, handleResetResult }) => {
       setActualQuestion(data.payload[0]);
 
       setAnswerArray(randomArray(data.payload[0]));
-
     }
     // Call the function for getiting the data from the back-end
     getData(topic);
@@ -45,33 +42,36 @@ const MainContent = ({ topic, handleAddPoints, handleResetResult }) => {
 
   // Function for Making and object answers from the question object into an array and randomize the positions
   function randomArray(obj) {
-  // Make and object with the question answers
-  const value = { 
-    a: obj.opta,
-    b: obj.optb,
-    c: obj.optc,
-    d: obj.answer       
-  }  
-  // Convert the object value into an array 
-  const answersArray = Object.values(value);  
-  // Function for changing the position of the values by random numbers 
+    // Make an object with the question answers
+    const value = {
+      a: obj.opta,
+      b: obj.optb,
+      c: obj.optc,
+      d: obj.answer,
+    };
+    // Convert the object value into an array
+    const answersArray = Object.values(value);
+    // Function for changing the position of the values by random numbers
     function randomizeArray(array) {
-      const element = array[array.length - 1]
+      const element = array[array.length - 1];
       const randomPosition = Math.floor(Math.random() * array.length);
-      const newArray = [...array.slice(0,randomPosition), element, ...array.slice(randomPosition, array.length -1)];
+      const newArray = [
+        ...array.slice(0, randomPosition),
+        element,
+        ...array.slice(randomPosition, array.length - 1),
+      ];
       return newArray;
     }
-   // Return the random array
-   return randomizeArray(answersArray);
-
+    // Return the random array
+    return randomizeArray(answersArray);
   }
 
   // Function for setting the disable state to false
   function handleDisable() {
     setDisable(false);
   }
-  // Function for setting the next question as actual question anda randomize the answers
-  function changeQuestion(){
+  // Function for setting the next question as actual question and randomise the answers
+  function changeQuestion() {
     setActualQuestion(questionsArray[position]);
     setAnswerArray(randomArray(questionsArray[position]));
   }
@@ -81,38 +81,47 @@ const MainContent = ({ topic, handleAddPoints, handleResetResult }) => {
   }
   // Function for checking the results of the answer when it has been clicked
   function checkResult(event) {
-    
     setDisable(true);
 
-    if ( event.target.innerText === actualQuestion.answer) {
-      setColor("green")
+    if (event.target.innerText === actualQuestion.answer) {
+      setColor("green");
       handleAddPoints();
       event.currentTarget.className = "green";
-    }else{
-       event.currentTarget.className = "red";
-       setColor("red");
-     }
+    } else {
+      event.currentTarget.className = "red";
+      setColor("red");
+    }
   }
 
   return (
-       <main>
-         <Quiz 
-               actualQuestion={actualQuestion}
-               disable={disable} 
-               position={position} 
-               handleResult={checkResult}
-               randomArray={answerArray} 
-               color={color}  
-               />
-        <NextQuestion handleChangeQuestion={changeQuestion} handleNextQuestion={handleNextQuestion} handleDisable={handleDisable} position={position}  />     
-        <Link className={position === 10 ? "button" : "button disable"} to="/results">See your sesult</Link>
-        <Link className="link" to="/" onClick={handleResetResult}>
-          <div className="homeButton">
-            <div id="homeTshirt"></div>
+    <main>
+      <Quiz
+        actualQuestion={actualQuestion}
+        disable={disable}
+        position={position}
+        handleResult={checkResult}
+        randomArray={answerArray}
+        color={color}
+      />
+      <NextQuestion
+        handleChangeQuestion={changeQuestion}
+        handleNextQuestion={handleNextQuestion}
+        handleDisable={handleDisable}
+        position={position}
+      />
+      <Link
+        className={position === 10 ? "button" : "button disable"}
+        to="/results"
+      >
+        See your result
+      </Link>
+      <Link className="link" to="/" onClick={handleResetResult}>
+        <div className="homeButton">
+          <div id="homeTshirt"></div>
         </div>
-        </Link>
-       </main>
-  )
+      </Link>
+    </main>
+  );
 };
 
 export default MainContent;
