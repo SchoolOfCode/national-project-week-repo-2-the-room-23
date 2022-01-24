5f03a2d7aeebdd9378307e47b393b25d07e469c9
Alex Platt <alja.platt@gmail.com>
2022-01-24 10:48:16 +0000

    merging

--- src/components/QuestionText/index.js
+++ src/components/QuestionText/index.js
@@ -1,17 +1,11 @@
 import "./style.css";
-<<<<<<< HEAD
-
-function Questions({ text }) {
-  return <p className="question-text">{text}</p>;
-=======
-// Function for rendering the questions 
+// Function for rendering the questions
 function Questions({ text, position }) {
-  return(
+  return (
     <div className="question">
       <span className="questionNumber">Question {position}/10</span>
       <p className="question-text">{text}</p>
     </div>
-  ) 
->>>>>>> alex
+  );
 }
 export default Questions;
